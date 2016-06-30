815e72db6bdd2d019eb9fb88a115d9018ce8d234
Rayna Harris <rayna.harris@utexas.edu>
2016-06-30 18:23:21 -0400

    perfected for loop

--- bin/mcmc.qpcr.stg.rmh.R
+++ bin/mcmc.qpcr.stg.rmh.R
@@ -8,12 +8,14 @@
 ## set path to data dir
 setwd("Z:/NSB_2016/IntegrativeNeuroscience/STGsingleneuron2015/data_xls")
 
+
 ## The process:
-## 1. Read raw data xlsx package then clean
-## 2. Wrangle standard curve data with dplyr and plyr package into a dataframe called dilutions
-## 3. Calculate primer efficiences with MCMC.qpcr PrimEFF function
-## 4. Wrangle sample info a with dplyr packag
-## 4. Analyze data with MCMC.qpcr package
+## 1. Loop over all experimental prjoect files and create one big "rawdata" dataframe
+## 2. Clean the data to make numbers numbers and rename important columns
+## 3. Wrangle standard curve data with dplyr and plyr package into a dataframe called dilutions
+## 4. Calculate primer efficiences with MCMC.qpcr PrimEFF function
+## 5. Wrangle sample info a with dplyr packag
+## 6. Analyze data with MCMC.qpcr package
 
 #install.packages("xlsx", dependencies = TRUE) #note, must have Java installed on computer
 #install.packages("dplyr")
@@ -24,46 +26,48 @@ library(dplyr)
 library(plyr)
 library(MCMC.qpcr)
 
-## 1. read raw data starting with row 42, then make quanitity a real number, then rename
-setwd("target_dir/")
 
-file_list <- list.files()
+## 1. Loop over all experimental prjoect files and create one big "rawdata" dataframe
+## uses read.xlsx function to read 1 sheet, sharting at row 8, nothing imported as a factor
+## NOTE! besure to clean enviornment before running this!
+
+file_list <- list.files() #creates a string will all the files in a diretory for us to loop through
 
 for (file in file_list){
   
   # if the merged dataset doesn't exist, create it
-  if (!exists("dataset")){
-    dataset <- read.table(file, header=TRUE, sep="\t")
+  if (!exists("rawdata")){
+    rawdata <- read.xlsx(file, sheetIndex = 1, startRow=8, stringsAsFactors=FALSE)
   }
   
   # if the merged dataset does exist, append to it
-  if (exists("dataset")){
-    temp_dataset <-read.table(file, header=TRUE, sep="\t")
-    dataset<-rbind(dataset, temp_dataset)
+  if (exists("rawdata")){
+    temp_dataset <-read.xlsx(file, sheetIndex = 1, startRow=8, stringsAsFactors=FALSE)
+    rawdata<-rbind(rawdata, temp_dataset)
     rm(temp_dataset)
   }
   
 }
 
-
-
-
-
-rawdata <- read.xlsx("071715_inx1inx2_data.xls", sheetIndex = 1, startRow=8, stringsAsFactors=FALSE)
 names(rawdata)
 str(rawdata)
-rawdata <- rename(rawdata, c("Sample.Name"="sample", "Target.Name"="gene",  "CÑ."="cq", "Quantity"="dna")) 
-names(rawdata)
-rawdata$dna <- as.numeric(rawdata$dna)
-rawdata$cq <- as.numeric(rawdata$cq, na.rm = TRUE)
+
+## 2. Clean data
+
+cleandata <- rawdata
+cleandata <- rename(cleandata, c("Sample.Name"="sample", "Target.Name"="gene",  "CÑ."="cq", "Quantity"="dna")) 
+names(cleandata)
+str(cleandata)
+cleandata$dna <- as.numeric(cleandata$dna, na.rm = TRUE)
+cleandata$cq <- as.numeric(cleandata$cq, na.rm = TRUE)
 
 
 
 ## 2. Create dilutions dataframe with quantiry, target name, and ct. 
 ##    Then rename for MCMC.qpcr
-dilutions <- rawdata %>%
+dilutions <- cleandata %>%
   filter(Task == "STANDARD") %>%
-  select(dna,  cq, gene)
+  select(dna, cq, gene) 
 str(dilutions)
 
 
@@ -73,7 +77,7 @@ eff <- PrimEff(dilutions) #creates a table with the primer efficiencies
 
 
 ## 4. create counts dataframe 
-counts <- rawdata %>%
+counts <- cleandata %>%
   filter(Task == "UNKNOWN") %>%
   select(sample, gene,  cq)
 str(counts)
